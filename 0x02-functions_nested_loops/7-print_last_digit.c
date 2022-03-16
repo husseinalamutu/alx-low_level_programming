aad7569af92d7625f191d42718baee221f4cc2fd
Hussein Alamutu <94724734+husseinalamutu@users.noreply.github.com>
2022-03-16 21:30:54 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,15 @@
 int print_last_digit(int n)
 {
 n = n % 10;
-if (n < 0)
+if (n >= 0)
 {
-n = -n;
-_putchar(n + '0');
 return (n);
 }
 else 
 { 
-return (n); 
+n = -n;
+_putchar(n + '0');
+return (n);
 }
 return (0);
 }
