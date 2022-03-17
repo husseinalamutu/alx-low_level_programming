bfcdbf9e420843d8f434e8ecf3f044f930273d69
husseinalamutu <husseinalamutu@gmail.com>
2022-03-17 23:30:18 +0100

    advanced

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,34 @@
+
+
+#include <stdio.h>
+ 
+/** Write a program that finds and prints 
+* the largest prime factor of the number 612852475143, 
+* followed by a new line
+*/
+
+int main()
+{
+  	int i, j, Number, isPrime, Highest; 
+   
+    	for (i = 2; i <= Number; i++)
+   	{
+     	if(Number % i == 0)
+        {
+   			isPrime = 1;
+			for (j = 2; j <= i/2; j++)
+			{
+				if(i % j == 0)
+				{
+					isPrime = 0;
+					break;
+				}
+			} 
+			if(isPrime == 1)
+			{
+			    Highest = i
+			}	          	
+		}
+   }
+  	return (Highest);
+}
