aab75511601aa3479cedda5410d9eb24ac0e2024
husseinalamutu <husseinalamutu@gmail.com>
2022-03-21 18:04:45 +0100

    pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/* swap_int - a function that swaps the value of two integers
+/**
+ * swap_int: a function that swaps the value of two integers
  * @a: the first integer
  * @b: the second integer
  * hold: the variable that holds var a
@@ -9,6 +10,7 @@
 void swap_int(int *a, int *b)
 {
 	int hold;
+
 	hold = *a;
 	*a = *b;
 	*b = hold;
