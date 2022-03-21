b62ef827f1a448cf46b5073f399d32103e6e87e8
husseinalamutu <husseinalamutu@gmail.com>
2022-03-21 19:18:58 +0100

    my commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
 * print_rev - prints a string, in reverse, followed by a new line.
@@ -15,7 +16,7 @@ int i;
 
 for (i = right_index; i >= left_index; i--)
 {
-_putchar(s[i]);
+_putchar (s[i]);
 }
-_putchar('\n');
+_putchar ('\n');
 }
