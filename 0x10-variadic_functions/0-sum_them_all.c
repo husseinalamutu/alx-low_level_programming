839e4f10ff4d09c41e5f81aa3c644800ce6e46fb
hussein alamutu <alamutuhussein@gmail.com>
2022-04-16 01:08:46 +0100

    print

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,12 @@ int sum_them_all(const unsigned int n, ...)
 
   else
   {
-  va_start (ap, n);         
-
+  va_start (ap, n);
+  }
   for (i = 0; i < n; i++)
-    sum += va_arg (ap, int);    
-
+  {
+    sum += va_arg (ap, int);
+  
   va_end (ap);                  
   return sum;
   }
